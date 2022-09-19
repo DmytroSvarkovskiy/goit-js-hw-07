b2a5488a20754f060fd90f82d17866ccb2d23e6a
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-09-19 21:30:45 +0300

    completed 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galaryDiv = document.querySelector('.gallery');
+
+const createGalery = collection => {
+    collection.map(({ preview, original, description }) => {
+        const markingElement = `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`;
+        galaryDiv.insertAdjacentHTML('afterbegin', markingElement);
+    })
+}
+
+createGalery(galleryItems)
+
+
+const onImgClick = event => {
+    event.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery a', {captionsData:'alt',captionDelay:250});
+}
+galaryDiv.addEventListener('click', onImgClick);
